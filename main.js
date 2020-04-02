6c299c66d6ae71fb49c3530d25f387b53adfc529
Adrian D. Alonzo <aalonzo3@gatech.edu>
2020-04-02 12:52:30 -0700

    Added 2 different screens, one for login and one for dashboard

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path');
 const fs = require('fs');
 
+const {ipcMain} = require('electron')
+
 const DOMAIN_URL = "instagram.com"
 const DOMAIN_ACTIVITY = "/accounts/activity"
 const DOMAIN_MESSAGES = "/direct/inbox"
@@ -17,15 +19,16 @@ const DOMAIN_SETTINGS = "/emails/settings"
 let mainWindow;
 let uploadButton;
 let isQuittingApp = false;
+let isLoggedIn = true;
 
-function createWindow () {
+function createLoginWindow () {
 
   const options = {
     width: 500,
     height: 600,
-    // resizable: false,
-    // fullscreen: false,
-    // maximizable: false,
+    resizable: false,
+    fullscreen: false,
+    maximizable: false,
     titleBarStyle: 'hiddenInset',
     title: 'InstaSend',
     webPreferences: {
@@ -44,23 +47,55 @@ function createWindow () {
 
 }
 
-function backgroundUpload() {
-  const hidden = ({
-    show: true
-  });
+function createDashboardWindow () {
 
-  invisibleWin = new BrowserWindow(hidden)
-  invisibleWin.loadURL('https://'+DOMAIN_URL)
+  const options = {
+    width: 800,
+    height: 950,
+    resizable: true,
+    maximizable: true,
+    titleBarStyle: 'hiddenInset',
+    title: 'InstaSend',
+    webPreferences: {
+			nodeIntegration: false
+		},
+  };
 
-  // force mobile (capture upload button for our website)
-  mainWindow.webContents.setUserAgent("Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19");
-  let output = invisibleWin.webContents
-  console.log(output)
+  // create window with settings
+  mainWindow = new BrowserWindow(options)
+  mainWindow.loadURL('https://'+DOMAIN_URL)
+
+  // apply css
+  mainWindow.webContents.on('dom-ready', () => {
+		mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/dash.css'), 'utf8'));
+	});
+
 }
+
+// function backgroundUpload() {
+//   const hidden = ({
+//     show: true
+//   });
+//
+//   invisibleWin = new BrowserWindow(hidden)
+//   invisibleWin.loadURL('https://'+DOMAIN_URL)
+//
+//   // force mobile (capture upload button for our website)
+//   invisibleWin.webContents.setUserAgent("Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19");
+//   invisibleWin.webContents.on('dom-ready', () => {
+//     // add cs to change our view
+// 		invisibleWin.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/dash.css'), 'utf8'));
+// 	});
+//   // let output = invisibleWin.webContents
+//   // console.log(output)
+// }
 
 // app.whenReady().then(createWindow)
 app.on('ready', function(){
   //open window
-  createWindow()
-  backgroundUpload()
+  if(!isLoggedIn){
+    createLoginWindow()
+  }else{
+    createDashboardWindow()
+  }
 });
