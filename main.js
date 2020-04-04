8733513946ea0e268384ce6a05b78e63b71d6e96
Adrian D. Alonzo <aalonzo3@gatech.edu>
2020-04-04 13:55:52 -0700

    Added dynamic sizing of window based on login or dashboard

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const path = require('path');
 const fs = require('fs');
-
 const {ipcMain} = require('electron')
 
 const DOMAIN_URL = "instagram.com"
@@ -16,86 +15,52 @@ const DOMAIN_SETTINGS_PRIVACY_SECURITY = "/accounts/privacy_and_security"
 const DOMAIN_LOGIN_ACTIVITY = "/session/login_activity"
 const DOMAIN_SETTINGS = "/emails/settings"
 
-let mainWindow;
-let uploadButton;
-let isQuittingApp = false;
-let isLoggedIn = true;
+let win;
 
-function createLoginWindow () {
+function createWindow () {
 
   const options = {
     width: 500,
-    height: 600,
+    height: 550,
     resizable: false,
-    fullscreen: false,
     maximizable: false,
+    movable: true,
     titleBarStyle: 'hiddenInset',
     title: 'InstaSend',
     webPreferences: {
-			nodeIntegration: false
+			nodeIntegration: true
 		},
   };
 
-  // create window with settings
-  mainWindow = new BrowserWindow(options)
-  mainWindow.loadURL('https://'+DOMAIN_URL)
+  // create main window
+  win = new BrowserWindow(options)
+  win.loadURL('https://'+DOMAIN_URL)
 
-  // apply css
-  mainWindow.webContents.on('dom-ready', () => {
-		mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/ig.css'), 'utf8'));
+  // check if we are at the login page
+  win.webContents.on('dom-ready', function (e) {
+    win.webContents.executeJavaScript(`
+      var ipcRenderer = require('electron').ipcRenderer;
+      var hasLogin = document.getElementsByClassName("gr27e")[0];
+      ipcRenderer.send('checkLogin', hasLogin);
+      console.log("Send checklogin");
+    `);
 	});
 
+  ipcMain.on('checkLogin', function(event, hasLogin){
+    console.log("Recv checklogin");
+    console.log(hasLogin);
+    if(!hasLogin){
+      win.resizable = true
+      win.maximizable = true
+      win.setSize(1100,950, true)
+      win.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/dash.css'), 'utf8'));
+    }else{
+      win.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/ig.css'), 'utf8'));
+    }
+
+  })
 }
 
-function createDashboardWindow () {
-
-  const options = {
-    width: 800,
-    height: 950,
-    resizable: true,
-    maximizable: true,
-    titleBarStyle: 'hiddenInset',
-    title: 'InstaSend',
-    webPreferences: {
-			nodeIntegration: false
-		},
-  };
-
-  // create window with settings
-  mainWindow = new BrowserWindow(options)
-  mainWindow.loadURL('https://'+DOMAIN_URL)
-
-  // apply css
-  mainWindow.webContents.on('dom-ready', () => {
-		mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/dash.css'), 'utf8'));
-	});
-
-}
-
-// function backgroundUpload() {
-//   const hidden = ({
-//     show: true
-//   });
-//
-//   invisibleWin = new BrowserWindow(hidden)
-//   invisibleWin.loadURL('https://'+DOMAIN_URL)
-//
-//   // force mobile (capture upload button for our website)
-//   invisibleWin.webContents.setUserAgent("Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19");
-//   invisibleWin.webContents.on('dom-ready', () => {
-//     // add cs to change our view
-// 		invisibleWin.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/dash.css'), 'utf8'));
-// 	});
-//   // let output = invisibleWin.webContents
-//   // console.log(output)
-// }
-
-// app.whenReady().then(createWindow)
 app.on('ready', function(){
-  //open window
-  if(!isLoggedIn){
-    createLoginWindow()
-  }else{
-    createDashboardWindow()
-  }
+  createWindow()
 });
