9ac847bf910297024166b815c42b4b0dcee4b9cc
Adrian D. Alonzo <aalonzo3@gatech.edu>
2020-12-20 17:35:19 -0800

    Applied css and updated readme

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ npm start run
 ```
 
 todo:
+[x] applied css for better user experience
 [ ] update window state upon login and expand
 [ ] allow color option modes
--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path');
 const fs = require('fs');
 
-
 let win;
 let loginForm;
 
@@ -11,9 +10,9 @@ function createWindow () {
   const options = {
     width: 500,
     height: 550,
-    resizable: true,
-    maximizable: true,
-    movable: true,
+    // resizable: true,
+    // maximizable: true,
+    // movable: true,
     titleBarStyle: 'hiddenInset',
     title: 'InstaSend',
     webPreferences: {
@@ -25,9 +24,36 @@ function createWindow () {
   mainWindow = new BrowserWindow(options)
   mainWindow.loadURL('https://instagram.com')
 
+  // handle close
+  // mainWindow.on('close', e => {
+  //   if(willQuitApp){
+  //     mainWindow = null;
+  //     return;
+  //   }
+  //   // check if user only tried to close window
+  //   e.preventDefault();
+  //   if(!mainWindow){
+  //     return;
+  //   }
+  //
+  //   if(!mainWindow.isFullScreen()){
+  //     mainWindow.hide();
+  //     return;
+  //   }
+  //
+  //   mainWindow.setFullScreen(false);
+  //   // wait fullscreen animation before hiding
+  //   setTimeout(
+  //     () => {
+  //       mainWindow.hide();
+  //     },
+  //     1000
+  //   );
+  // });
+
   mainWindow.webContents.on('dom-ready', function (e) {
     mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/ig.css'), 'utf8'));
-  })
+  });
 
   // check if we are at the login page //dom-ready
   // win.webContents.on('dom-ready', function (e) {
