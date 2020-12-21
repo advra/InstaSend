501dfe22aa922bb3c1ff8413ff487d90742a60a3
Adrian D. Alonzo <aalonzo3@gatech.edu>
2020-12-21 00:59:03 -0800

    added osx lifecycle window handling

--- main.js
+++ main.js
@@ -1,57 +1,126 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path');
 const fs = require('fs');
-// const windowStateKeeper = require('electron-window-state')
+const windowStateKeeper = require('electron-window-state');
 
 let win;
-let loginForm;
+let mainWindow;
+var hasLoginForm = false;
+let willQuitApp = false;
 
 function createWindow () {
 
+  let mainWindowState = windowStateKeeper({
+		defaultWidth: 500,
+		defaultHeight: 600,
+	});
+
   const options = {
-    width: 500,
-    height: 550,
-    // resizable: true,
-    // maximizable: true,
+    minWidth: 500,
+    minHeight: 600,
+    x: mainWindowState.x,
+		y: mainWindowState.y,
+		width: mainWindowState.width,
+		height: mainWindowState.height,
+    resizable: true,
+    maximizable: true,
     // movable: true,
+    backgroundColor: '#FFF', // enable subpixel anti-aliasing
     frame: false,
     titleBarStyle: 'hidden',
     title: 'InstaSend',
     webPreferences: {
-			nodeIntegration: false
+      nodeIntegration: false
 		},
   };
-
+  
   // create window with settings
-  mainWindow = new BrowserWindow(options)
+  mainWindow = new BrowserWindow(options);
+  mainWindowState.manage(mainWindow);
+  mainWindow.webContents.openDevTools();
   mainWindow.loadURL('https://instagram.com')
 
-  mainWindow.webContents.on('dom-ready', function (e) {
+  
+  mainWindow.webContents.once('dom-ready', ()=> {
     mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/ig.css'), 'utf8'));
   });
+  
+  // handling close
+  mainWindow.on('close', e => {
+		if (willQuitApp) {
+			// the user tried to quit the app
+			mainWindow = null;
+			return;
+		}
 
-  // check if we are at the login page //dom-ready
-  // win.webContents.on('dom-ready', function (e) {
-  //   win.webContents.executeJavaScript(`
-  //     var ipcRenderer = require('electron').ipcRenderer;
-  //     var hasLogin = document.getElementsByClassName("gr27e")[0];
-  //     ipcRenderer.send('checkLogin', hasLogin);
-  //     console.log("Send checklogin");
-  //   `);
-	// });
-  //
-  // ipcMain.on('checkLogin', function(event, hasLogin){
-  //   console.log("Recv checklogin");
-  //   if(!hasLogin){
-  //     win.resizable = true
-  //     win.maximizable = true
-  //     win.setSize(1100,950, true)
-  //     win.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/dash.css'), 'utf8'));
-  //   }else{
-  //     win.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/ig.css'), 'utf8'));
-  //   }
-  //
-  // })
+		// the user only tried to close the window
+		e.preventDefault();
+		if (!mainWindow) {
+			return;
+		}
+
+		if (!mainWindow.isFullScreen()) {
+			mainWindow.hide();
+			return;
+		}
+
+		mainWindow.setFullScreen(false);
+		// Wait for full screen animation to finish before hiding
+		setTimeout(
+			() => {
+				mainWindow.hide();
+			},
+			1000
+		);
+	});
+
+  mainWindow.webContents.on('new-window', (event, url, frameName, disposition, options) => {
+		event.preventDefault();
+
+		if (url === 'about:blank') {
+			if (frameName !== 'about:blank') {
+				options.titleBarStyle = 'default';
+				options.webPreferences.nodeIntegration = false;
+				event.newGuest = new electron.BrowserWindow(options);
+			}
+		} else {
+			shell.openExternal(url);
+		}
+	});
 }
 
+// app events
 app.on('ready', createWindow);
+
+app.on('before-quit', () => willQuitApp = true);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', function() {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow) {
+		mainWindow.show();
+	} else {
+		createWindow();
+	}
+});
+
+// app.on('haschange', ()=> {
+//   mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, '/assets/ig.css'), 'utf8'));
+//   mainWindow.webContents.executeJavaScript(`
+//   hasLoginForm = document.getElementsByClassName("gr27e")[0];
+
+//   if(!hasLoginForm){
+//     console.log("no login");
+//   }else{
+//     console.log("login found");
+//   }
+
+// `)
+// });
