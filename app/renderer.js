bf970bbcd274fddf477b1bcdd9404198b6361ef6
Adrian D. Alonzo <aalonzo3@gatech.edu>
2020-12-23 13:20:08 -0800

    Correct webview double scrollbars

--- app/renderer.js
+++ app/renderer.js
@@ -3,11 +3,11 @@
 // All of the Node.js APIs are available in this process.
 console.log(`Loading renderer.js...`);
 
-if (process.platform !== 'darwin') {
+if (process.platform == 'darwin') {
     document.body.innerHTML = `<h1>MAC</h1><webview src="https://www.instagram.com/" style="width:100%; height:100%"  preload="ig-preload.js" ></webview>`
 }
 else{
-    document.body.innerHTML = `<h1>WINDOWS/LINUX</h1><webview src="https://www.instagram.com/" style="width:100%; height:100%"  preload="ig-preload.js" ></webview>`
+    document.body.innerHTML = `<h1>WINDOWS/LINUX</h1><webview src="https://www.instagram.com/" style="width:100%; height:100%; overflow-x: hidden; " preload="ig-preload.js" ></webview>`
 }
 
 const webview = document.querySelector("webview");
